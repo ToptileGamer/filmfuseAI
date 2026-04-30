187f91918b6be43df6d234890e713946c2ff85a4
J Gautham <jgautham42@gmail.com>
2026-04-30 23:28:00 +0530

    Revert "new try"
    This reverts commit 8649951275ee4df684b39871875b62d18697fb7e.

--- firebase.js
+++ firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -14,10 +13,11 @@ import {
   getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+// 🔥 YOUR CONFIG HERE
 const firebaseConfig = {
   apiKey: "AIzaSyAAOa7HoUIq_XtYoZG_s4gUs1E6k1g3M_w",
   authDomain: "https://filmfuseai-e3452.firebaseapp.com/",
-  projectId: "https://filmfuseai-e3452-default-rtdb.firebaseio.com"
+  projectId: "https://console.firebase.google.com/u/0/project/filmfuseai-e3452/database/filmfuseai-e3452-default-rtdb/data/",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -25,25 +25,34 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-export const signup = (email, pass) =>
-  createUserWithEmailAndPassword(auth, email, pass);
+// SIGNUP
+export async function signup(email, password) {
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
 
-export const login = (email, pass) =>
-  signInWithEmailAndPassword(auth, email, pass);
+// LOGIN
+export async function login(email, password) {
+  return await signInWithEmailAndPassword(auth, email, password);
+}
 
-export const logout = () => signOut(auth);
+// LOGOUT
+export function logout() {
+  return signOut(auth);
+}
 
-export async function saveWatchlist(uid, movies) {
+// SAVE WATCHLIST
+export async function saveWatchlist(userId, movies) {
   await addDoc(collection(db, "watchlists"), {
-    uid,
+    userId,
     movies,
     createdAt: new Date()
   });
 }
 
-export async function getWatchlist(uid) {
-  const snap = await getDocs(collection(db, "watchlists"));
-  return snap.docs
-    .map(d => d.data())
-    .filter(x => x.uid === uid);
+// GET WATCHLIST
+export async function getWatchlist(userId) {
+  const snapshot = await getDocs(collection(db, "watchlists"));
+  return snapshot.docs
+    .map(doc => doc.data())
+    .filter(item => item.userId === userId);
 }
